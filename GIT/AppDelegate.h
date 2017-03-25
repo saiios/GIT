e874f12e4271be7ec05b6ba3081f0b9d27c6a2a1
saiios <saikrishna4477@gmail.com>
2017-03-25 12:38:16 +0530

    Initial Commit

---
+++ GIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIT
+//
+//  Created by INDOBYTES on 25/03/17.
+//  Copyright © 2017 sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIT
+//
+//  Created by INDOBYTES on 25/03/17.
+//  Copyright © 2017 sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
